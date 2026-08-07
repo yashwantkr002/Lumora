6487e06bf93e6c2e2e22034bf739dfdaa4adbdd1
Yashwant Das <yashwantkr002@gmail.com>
2026-08-07 17:16:15 +0530

    fix changes

--- Lumora/wsgi.py
+++ Lumora/wsgi.py
@@ -1,6 +1,12 @@
 
 
 import os
+from pathlib import Path
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 from django.core.wsgi import get_wsgi_application
 
--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 def main():
