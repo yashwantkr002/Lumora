b3fbc565d694cfbd94bca5ab9cd6ad0fef043e1d
Yashwant Das <yashwantkr002@gmail.com>
2026-08-08 08:57:40 +0530

    loffing

--- Lumora/settings/production.py
+++ Lumora/settings/production.py
@@ -17,3 +17,26 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 SECURE_HSTS_PRELOAD = True
+
+# Lumora/settings/production.py ke bottom mein paste karo
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
